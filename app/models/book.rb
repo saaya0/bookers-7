291b64fa3f36c78a3533d024db5e6af33d8ebc70
SaayaShimada <93375049+saaya0@users.noreply.github.com>
2021-12-10 09:06:28 +0900

    応用課題7ab

---
+++ app/models/book.rb
@@ -0,0 +1,19 @@
+class Book < ApplicationRecord
+	belongs_to :user
+	has_many :book_comments, dependent: :destroy
+	has_many :favorites, dependent: :destroy
+	has_many :favorited_users, through: :favorites, source: :user
+
+	scope :created_today, -> { where(created_at: Time.zone.now.all_day) }
+	scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) }
+	scope :created_week, -> { where(created_at: 1.week.ago.beginning_of_day..Time.zone.now.end_of_day) }
+	scope :created_weeks, -> { where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day) }
+
+
+	def favorited_by?(user)
+		favorites.where(user_id: user.id).exists?
+	end
+
+	validates :title, presence: true
+	validates :body, presence: true, length: {maximum: 200}
+end
